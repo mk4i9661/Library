17bbb2af89f3b8bce800fd1e97d40043500ac2f7
Dmitry <mk4i9661@hotmail.com>
2013-10-27 02:40:27 +0400

    cards tab has been added

--- Library/Library.WindowsClient/MainForm.Designer.cs
+++ Library/Library.WindowsClient/MainForm.Designer.cs
@@ -11,6 +11,7 @@ namespace Library.WindowsClient
         private void InitializeComponent() {
             this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
             this.rpReaders = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.rpCards = new DevExpress.XtraBars.Ribbon.RibbonPage();
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -18,14 +19,14 @@ namespace Library.WindowsClient
             // 
             this.ribbonControl1.AllowMinimizeRibbon = false;
             this.ribbonControl1.ExpandCollapseItem.Id = 0;
-            this.ribbonControl1.ExpandCollapseItem.Name = "";
             this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbonControl1.ExpandCollapseItem});
             this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
             this.ribbonControl1.MaxItemId = 1;
             this.ribbonControl1.Name = "ribbonControl1";
             this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
-            this.rpReaders});
+            this.rpReaders,
+            this.rpCards});
             this.ribbonControl1.ShowToolbarCustomizeItem = false;
             this.ribbonControl1.Size = new System.Drawing.Size(1200, 153);
             this.ribbonControl1.Toolbar.ShowCustomizeItem = false;
@@ -34,6 +35,11 @@ namespace Library.WindowsClient
             // 
             this.rpReaders.Name = "rpReaders";
             this.rpReaders.Text = "Читатели";
+            // 
+            // rpCards
+            // 
+            this.rpCards.Name = "rpCards";
+            this.rpCards.Text = "Читательские карты";
             // 
             // MainForm
             // 
@@ -49,5 +55,6 @@ namespace Library.WindowsClient
 
         private DevExpress.XtraBars.Ribbon.RibbonPage rpReaders;
         private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage rpCards;
     }
 }
