aa46a2a3eb0b9cda086de7068455f956770bf26c
BlackieLawless <keng.91@mail.ru>
2013-11-13 21:06:22 +0400

    частично сделана вкладка читателей 2.0

--- Library/Library.WindowsClient/EditForms/ReaderEditForm.Designer.cs
+++ Library/Library.WindowsClient/EditForms/ReaderEditForm.Designer.cs
@@ -34,10 +34,12 @@
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.teMiddleName = new Library.UI.DevExpressControls.Controls.LibraryTextEdit();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.teAdress = new Library.UI.DevExpressControls.Controls.LibraryTextEdit();
+            this.teAddress = new Library.UI.DevExpressControls.Controls.LibraryTextEdit();
             this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
             this.tePhone = new Library.UI.DevExpressControls.Controls.LibraryTextEdit();
             this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.seId = new Library.UI.DevExpressControls.Controls.LibrarySpinEdit();
+            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).BeginInit();
@@ -48,25 +50,28 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.teMiddleName.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.teAdress.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teAddress.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tePhone.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seId.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.seId);
             this.layoutControl1.Controls.Add(this.tePhone);
-            this.layoutControl1.Controls.Add(this.teAdress);
+            this.layoutControl1.Controls.Add(this.teAddress);
             this.layoutControl1.Controls.Add(this.teMiddleName);
             this.layoutControl1.Controls.Add(this.teLastName);
             this.layoutControl1.Controls.Add(this.teName);
             this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(314, 174, 362, 350);
-            this.layoutControl1.Size = new System.Drawing.Size(378, 141);
+            this.layoutControl1.Size = new System.Drawing.Size(378, 165);
             // 
             // layoutControl2
             // 
-            this.layoutControl2.Location = new System.Drawing.Point(0, 141);
+            this.layoutControl2.Location = new System.Drawing.Point(0, 165);
             // 
             // layoutControlGroup1
             // 
@@ -77,17 +82,18 @@
             this.layoutControlItem1,
             this.layoutControlItem3,
             this.layoutControlItem4,
-            this.layoutControlItem5});
-            this.layoutControlGroup1.Size = new System.Drawing.Size(378, 141);
+            this.layoutControlItem5,
+            this.layoutControlItem6});
+            this.layoutControlGroup1.Size = new System.Drawing.Size(378, 165);
             // 
             // teName
             // 
-            this.teName.Location = new System.Drawing.Point(74, 12);
+            this.teName.Location = new System.Drawing.Point(90, 36);
             this.teName.Name = "teName";
             this.teName.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
             this.teName.Properties.Appearance.Options.UseFont = true;
             this.teName.Properties.MaxLength = 255;
-            this.teName.Size = new System.Drawing.Size(292, 20);
+            this.teName.Size = new System.Drawing.Size(276, 20);
             this.teName.StyleController = this.layoutControl1;
             this.teName.TabIndex = 4;
             // 
@@ -95,20 +101,20 @@
             // 
             this.layoutControlItem1.Control = this.teName;
             this.layoutControlItem1.CustomizationFormText = "Имя:";
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 24);
             this.layoutControlItem1.Name = "layoutControlItem1";
             this.layoutControlItem1.Size = new System.Drawing.Size(358, 24);
             this.layoutControlItem1.Text = "Имя:";
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(58, 14);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(74, 14);
             // 
             // teLastName
             // 
-            this.teLastName.Location = new System.Drawing.Point(74, 36);
+            this.teLastName.Location = new System.Drawing.Point(90, 60);
             this.teLastName.Name = "teLastName";
             this.teLastName.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
             this.teLastName.Properties.Appearance.Options.UseFont = true;
             this.teLastName.Properties.MaxLength = 255;
-            this.teLastName.Size = new System.Drawing.Size(292, 20);
+            this.teLastName.Size = new System.Drawing.Size(276, 20);
             this.teLastName.StyleController = this.layoutControl1;
             this.teLastName.TabIndex = 5;
             // 
@@ -116,20 +122,20 @@
             // 
             this.layoutControlItem2.Control = this.teLastName;
             this.layoutControlItem2.CustomizationFormText = "Фамилия:";
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 48);
             this.layoutControlItem2.Name = "layoutControlItem2";
             this.layoutControlItem2.Size = new System.Drawing.Size(358, 24);
             this.layoutControlItem2.Text = "Фамилия:";
-            this.layoutControlItem2.TextSize = new System.Drawing.Size(58, 14);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(74, 14);
             // 
             // teMiddleName
             // 
-            this.teMiddleName.Location = new System.Drawing.Point(74, 60);
+            this.teMiddleName.Location = new System.Drawing.Point(90, 84);
             this.teMiddleName.Name = "teMiddleName";
             this.teMiddleName.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
             this.teMiddleName.Properties.Appearance.Options.UseFont = true;
             this.teMiddleName.Properties.MaxLength = 255;
-            this.teMiddleName.Size = new System.Drawing.Size(292, 20);
+            this.teMiddleName.Size = new System.Drawing.Size(276, 20);
             this.teMiddleName.StyleController = this.layoutControl1;
             this.teMiddleName.TabIndex = 6;
             // 
@@ -137,41 +143,41 @@
             // 
             this.layoutControlItem3.Control = this.teMiddleName;
             this.layoutControlItem3.CustomizationFormText = "Отчество:";
-            this.layoutControlItem3.Location = new System.Drawing.Point(0, 48);
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 72);
             this.layoutControlItem3.Name = "layoutControlItem3";
             this.layoutControlItem3.Size = new System.Drawing.Size(358, 24);
             this.layoutControlItem3.Text = "Отчество:";
-            this.layoutControlItem3.TextSize = new System.Drawing.Size(58, 14);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(74, 14);
             // 
-            // teAdress
+            // teAddress
             // 
-            this.teAdress.Location = new System.Drawing.Point(74, 84);
-            this.teAdress.Name = "teAdress";
-            this.teAdress.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
-            this.teAdress.Properties.Appearance.Options.UseFont = true;
-            this.teAdress.Properties.MaxLength = 255;
-            this.teAdress.Size = new System.Drawing.Size(292, 20);
-            this.teAdress.StyleController = this.layoutControl1;
-            this.teAdress.TabIndex = 7;
+            this.teAddress.Location = new System.Drawing.Point(90, 108);
+            this.teAddress.Name = "teAddress";
+            this.teAddress.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.teAddress.Properties.Appearance.Options.UseFont = true;
+            this.teAddress.Properties.MaxLength = 255;
+            this.teAddress.Size = new System.Drawing.Size(276, 20);
+            this.teAddress.StyleController = this.layoutControl1;
+            this.teAddress.TabIndex = 7;
             // 
             // layoutControlItem4
             // 
-            this.layoutControlItem4.Control = this.teAdress;
+            this.layoutControlItem4.Control = this.teAddress;
             this.layoutControlItem4.CustomizationFormText = "Адрес:";
-            this.layoutControlItem4.Location = new System.Drawing.Point(0, 72);
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 96);
             this.layoutControlItem4.Name = "layoutControlItem4";
             this.layoutControlItem4.Size = new System.Drawing.Size(358, 24);
             this.layoutControlItem4.Text = "Адрес:";
-            this.layoutControlItem4.TextSize = new System.Drawing.Size(58, 14);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(74, 14);
             // 
             // tePhone
             // 
-            this.tePhone.Location = new System.Drawing.Point(74, 108);
+            this.tePhone.Location = new System.Drawing.Point(90, 132);
             this.tePhone.Name = "tePhone";
             this.tePhone.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
             this.tePhone.Properties.Appearance.Options.UseFont = true;
             this.tePhone.Properties.MaxLength = 255;
-            this.tePhone.Size = new System.Drawing.Size(292, 20);
+            this.tePhone.Size = new System.Drawing.Size(276, 20);
             this.tePhone.StyleController = this.layoutControl1;
             this.tePhone.TabIndex = 8;
             // 
@@ -179,11 +185,45 @@
             // 
             this.layoutControlItem5.Control = this.tePhone;
             this.layoutControlItem5.CustomizationFormText = "Телефон:";
-            this.layoutControlItem5.Location = new System.Drawing.Point(0, 96);
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 120);
             this.layoutControlItem5.Name = "layoutControlItem5";
             this.layoutControlItem5.Size = new System.Drawing.Size(358, 25);
             this.layoutControlItem5.Text = "Телефон:";
-            this.layoutControlItem5.TextSize = new System.Drawing.Size(58, 14);
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(74, 14);
+            // 
+            // seId
+            // 
+            this.seId.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.seId.Location = new System.Drawing.Point(90, 12);
+            this.seId.Name = "seId";
+            this.seId.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.seId.Properties.Appearance.Options.UseFont = true;
+            this.seId.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.seId.Properties.IsFloatValue = false;
+            this.seId.Properties.Mask.EditMask = "F0";
+            this.seId.Properties.MaxValue = new decimal(new int[] {
+            2000000000,
+            0,
+            0,
+            0});
+            this.seId.Size = new System.Drawing.Size(276, 20);
+            this.seId.StyleController = this.layoutControl1;
+            this.seId.TabIndex = 9;
+            // 
+            // layoutControlItem6
+            // 
+            this.layoutControlItem6.Control = this.seId;
+            this.layoutControlItem6.CustomizationFormText = "№ паспорта:";
+            this.layoutControlItem6.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem6.Name = "layoutControlItem6";
+            this.layoutControlItem6.Size = new System.Drawing.Size(358, 24);
+            this.layoutControlItem6.Text = "№ паспорта:";
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(74, 14);
             // 
             // ReaderEditForm
             // 
@@ -191,7 +231,7 @@
             this.Appearance.Options.UseFont = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 194);
+            this.ClientSize = new System.Drawing.Size(378, 218);
             this.Name = "ReaderEditForm";
             this.Text = "Читатель";
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
@@ -204,10 +244,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.teMiddleName.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.teAdress.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teAddress.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tePhone.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seId.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -217,12 +259,14 @@
         private UI.DevExpressControls.Controls.LibraryTextEdit teName;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
         private UI.DevExpressControls.Controls.LibraryTextEdit tePhone;
-        private UI.DevExpressControls.Controls.LibraryTextEdit teAdress;
+        private UI.DevExpressControls.Controls.LibraryTextEdit teAddress;
         private UI.DevExpressControls.Controls.LibraryTextEdit teMiddleName;
         private UI.DevExpressControls.Controls.LibraryTextEdit teLastName;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private UI.DevExpressControls.Controls.LibrarySpinEdit seId;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
     }
 }
